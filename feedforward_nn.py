4a63f3e2fde8f985e53ff61002afee990c87b1a2
Adel Mandanas <mandanas.adel@gmail.com>
2017-11-28 21:08:48 +0800

    Add Introduction and Methodology to the notebook

--- feedforward_nn.py
+++ feedforward_nn.py
@@ -1,5 +1,7 @@
 from sklearn.datasets import fetch_mldata
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+
 import time
 
 # Fetch the MNIST dataset
@@ -8,13 +10,12 @@ print("Successfully fetched MNIST dataset")
 
 # Separate the dataset into training and testing
 X, y = mnist.data / 255., mnist.target
-X_train, X_test = X[:60000], X[60000:]
-y_train, y_test = y[:60000], y[60000:]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.14285, random_state=42)
 
 start = time.time()
 
 # Build the feedforward neural network
-mlp = MLPClassifier(hidden_layer_sizes=(100,), activation='logistic', max_iter=100, alpha=1e-4, solver='sgd', verbose=10, tol=1e-4, random_state=1, learning_rate_init=0.1)
+mlp = MLPClassifier(hidden_layer_sizes=(100,), activation='relu', max_iter=100, alpha=1e-4, solver='sgd', verbose=10, tol=1e-4, random_state=1, learning_rate_init=0.1)
 
 # Train the feedforward neural network
 mlp.fit(X_train, y_train)
